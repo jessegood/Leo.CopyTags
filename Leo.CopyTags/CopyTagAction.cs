969c7cb2f3699041782581245e500f879e76eb6f
jessegood <gdjss2728@gmail.com>
2017-04-21 17:28:37 +0900

    Add project files.

---
+++ Leo.CopyTags/CopyTagAction.cs
@@ -0,0 +1,43 @@
+﻿namespace Leo.CopyTags
+{
+    using Sdl.Desktop.IntegrationApi;
+    using Sdl.Desktop.IntegrationApi.Extensions;
+    using Sdl.FileTypeSupport.Framework.BilingualApi;
+    using Sdl.TranslationStudioAutomation.IntegrationApi;
+    using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+    using System;
+    using System.Collections.Generic;
+    using System.Windows.Forms;
+
+    [Action("CopyTagAction", Icon = "Brackets", Name = "Copy Tags to Target")]
+    [ActionLayout(typeof(TranslationStudioDefaultContextMenus.EditorDocumentContextMenuLocation), 1, DisplayType.Large)]
+    [Shortcut(Keys.Alt | Keys.T)]
+    public class CopyTagAction : AbstractViewControllerAction<EditorController>
+    {
+        protected override void Execute()
+        {
+            var currentSegment = Controller?.ActiveDocument?.GetActiveSegmentPair();
+
+            if (currentSegment != null && currentSegment.Target.Count == 0)
+            {
+                var source = currentSegment.Source;
+                var tags = GetTags(source);
+
+                foreach (var tag in tags)
+                {
+                    currentSegment.Target.Add((IAbstractMarkupData)tag.Clone());
+                }
+            }
+
+            Controller.ActiveDocument.UpdateSegmentPair(currentSegment);
+        }
+
+        private IList<IAbstractMarkupData> GetTags(ISegment source)
+        {
+            var tagVisitor = new TagVisitor();
+            tagVisitor.VisitSegment((ISegment)source.Clone());
+
+            return tagVisitor.Tags;
+        }
+    }
+}
---
+++ Leo.CopyTags/TagVisitor.cs
@@ -0,0 +1,92 @@
+﻿namespace Leo.CopyTags
+{
+    using Sdl.FileTypeSupport.Framework.BilingualApi;
+    using System.Collections.Generic;
+
+    internal class TagVisitor : IMarkupDataVisitor
+    {
+        public IList<IAbstractMarkupData> Tags { get; private set; } = new List<IAbstractMarkupData>();
+
+        public void VisitCommentMarker(ICommentMarker commentMarker)
+        {
+        }
+
+        public void VisitLocationMarker(ILocationMarker location)
+        {
+        }
+
+        public void VisitLockedContent(ILockedContent lockedContent)
+        {
+        }
+
+        public void VisitOtherMarker(IOtherMarker marker)
+        {
+        }
+
+        public void VisitPlaceholderTag(IPlaceholderTag tag)
+        {
+            Tags.Add(tag);
+        }
+
+        public void VisitRevisionMarker(IRevisionMarker revisionMarker)
+        {
+        }
+
+        public void VisitSegment(ISegment segment)
+        {
+            VisitChildren(segment);
+
+            foreach (var tag in Tags)
+            {
+                if (tag is ITagPair)
+                {
+                    ClearContentInTagPairs((ITagPair)tag);
+                }
+            }
+        }
+
+        public void VisitTagPair(ITagPair tagPair)
+        {
+            Tags.Add(tagPair);
+        }
+
+        public void VisitText(IText text)
+        {
+        }
+
+        private static bool IsRemovableItem(IAbstractMarkupData item)
+        {
+            return typeof(IText).IsAssignableFrom(item.GetType()) ||
+                   typeof(ICommentMarker).IsAssignableFrom(item.GetType()) ||
+                   typeof(ILocationMarker).IsAssignableFrom(item.GetType()) ||
+                   typeof(ILockedContent).IsAssignableFrom(item.GetType()) ||
+                   typeof(IOtherMarker).IsAssignableFrom(item.GetType()) ||
+                   typeof(IRevisionMarker).IsAssignableFrom(item.GetType());
+        }
+
+        private void ClearContentInTagPairs(ITagPair tagPair)
+        {
+            for (int i = tagPair.Count - 1; i >= 0; --i)
+            {
+                var item = tagPair[i];
+
+                if (IsRemovableItem(item))
+                {
+                    item.RemoveFromParent();
+                }
+                else if (item is ITagPair)
+                {
+                    ClearContentInTagPairs((ITagPair)item);
+                }
+            }
+        }
+
+        private void VisitChildren(IAbstractMarkupDataContainer container)
+        {
+            foreach (var item in container)
+            {
+                item.AcceptVisitor(this);
+            }
+        }
+    }
+}
